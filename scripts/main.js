3f90cb1d4ab7f9300e8a524cb0ba1371ac2c8761
Bryan <bryan.legras@gmail.com>
2021-05-12 14:49:07 +0200

    Stopwatch OK

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,60 @@
+// TODO : test if window.onload is obligatory
 window.onload = function () {
 
-const tens = document.getElementById('tens');
+const tensHTML = document.getElementById('tens');
+const secondsHTML = document.getElementById('seconds');
 
-tens.textContent ="01";
+let tens = 00;
+let seconds = 00;
+
+const buttonStart = document.getElementById('button__start');
+const buttonStop = document.getElementById('button__stop');
+const buttonReset = document.getElementById('button__reset');
+
+var Interval;
+
+buttonStart.onclick = function() {
+
+    clearInterval(Interval);
+    Interval = setInterval(runTimer,10);
+}
+
+buttonStop.onclick = function() {
+    clearInterval(Interval);
+}
+
+buttonReset.onclick = function() {
+
+    clearInterval(Interval);
+    tens = 00;
+    seconds = 00;
+
+    // TODO : MAKE this in a fonction ( call many times )
+    tensHTML.textContent = "00";
+    secondsHTML.textContent = "00";
+}
+
+function runTimer() {
+
+    tens++;
+
+    if(tens < 10){
+        tensHTML.textContent = "0" + tens;
+    }else{
+        tensHTML.textContent = tens;
+    }
+
+    if(tens == 100){
+        tens = 00;
+        seconds ++;
+
+        if(seconds < 10){
+            secondsHTML.textContent = "0" + seconds;
+        }else{
+            secondsHTML.textContent = seconds;
+        }
+    }
+
+}
 
 }
